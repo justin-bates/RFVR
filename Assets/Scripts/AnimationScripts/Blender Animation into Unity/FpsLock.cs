75d6749afa6395d1cff43b2bd7be37e1377295e7
KP Pak <79285326+AceCharms@users.noreply.github.com>
2023-07-27 15:42:53 -0400

    Saving Blender Blocking Animation into Unity
    I directly saved the Blender Blocking Animation file into Unity
    (These are unbaked animations)

---
+++ Assets/Scripts/AnimationScripts/Blender Animation into Unity/FpsLock.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FpsLock : MonoBehaviour
+{
+    void Start()
+    {
+        Application.targetFrameRate = 24;
+    }
+
+    void Update()
+    {
+        
+    }
+}
