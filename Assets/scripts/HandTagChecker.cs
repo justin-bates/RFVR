db66e3bc50944cc7e1f6add1d0e749712bcff7e9
justin-bates <135681445+justin-bates@users.noreply.github.com>
2023-08-07 10:53:29 -0400

    changes
    change

---
+++ Assets/scripts/HandTagChecker.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class HandTagChecker : MonoBehaviour
+{
+    XRDirectInteractor interactor;
+
+    void Awake()
+    {
+        interactor = GetComponent<XRDirectInteractor>();
+        interactor.hoverEntered.AddListener(CheckHandTag);
+    }
+
+    void CheckHandTag(HoverEnterEventArgs args)
+    {
+        if (args.interactorObject.interactor.CompareTag("rHand"))
+        {
+            Debug.Log("This is the hand you're looking for");
+        }
+    }
+}
