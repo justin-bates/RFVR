27447cb56ecb1c43813890ada1b49385acb62bfb
justin-bates <135681445+justin-bates@users.noreply.github.com>
2023-08-08 13:35:09 -0400

    Reorganizing KP's Assets Folder
    -Delete all of the PlayerVR Prefab on each scene
    -change the LoadtoNextScene Script by adding in space of the scene names

--- Assets/KPs ArtAssets/Scripts_MyArtAssets/AnimationScripts/LoadNextScene Animation Scripts/Act1Scene.cs
+++ Assets/KPs ArtAssets/Scripts_MyArtAssets/AnimationScripts/LoadNextScene Animation Scripts/Act1Scene.cs
@@ -3,13 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Act2Scene : MonoBehaviour
+public class Act1Scene : MonoBehaviour
     //Make sure to match your LoadNextScene Script name to your current Scene.
 {
     void OnEnable()
     {
         //Only specifying the sceneName or sceneBuildIndex will load the Scene with the Single mode
-        SceneManager.LoadScene("Act3Scene", LoadSceneMode.Single);
+        SceneManager.LoadScene("Act 2 Scene", LoadSceneMode.Single);
         //In between the "_" you type in the next scene you wanted to load.
     }
 }
--- Assets/KPs ArtAssets/Scripts_MyArtAssets/AnimationScripts/LoadNextScene Animation Scripts/Act2Scene.cs
+++ Assets/KPs ArtAssets/Scripts_MyArtAssets/AnimationScripts/LoadNextScene Animation Scripts/Act2Scene.cs
@@ -3,13 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Act1Scene : MonoBehaviour
+public class Act2Scene : MonoBehaviour
     //Make sure to match your LoadNextScene Script name to your current Scene.
 {
     void OnEnable()
     {
         //Only specifying the sceneName or sceneBuildIndex will load the Scene with the Single mode
-        SceneManager.LoadScene("Act2Scene", LoadSceneMode.Single);
+        SceneManager.LoadScene("Act 3 Scene", LoadSceneMode.Single);
         //In between the "_" you type in the next scene you wanted to load.
     }
 }
